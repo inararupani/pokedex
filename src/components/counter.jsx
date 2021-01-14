ddacb4d4eb6b2b440ff37e6cf37e24b5def93e71
Inara Rupani <inara.rupani@gmail.com>
2021-01-14 01:24:38 -0600

    Build upon counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    //dynamic object that have attributes
+    state  = {
+        count: 0
+    };
     render() { 
         return (
             //React.Fragment - removes extra wrap of div in web browser (will get extra div wrap by using <div><\div> tab)
         <React.Fragment> 
-            <h1>Hello World</h1>
+            <span> {this.formatCount()} </span> 
             <button>Increment</button>
         </React.Fragment>
+        //this - current object; dynamic rendering
+        //{} - can have any value - either hardcoded or function return
         
         );
+    }
+
+    formatCount(){
+        const {count} = this.state; //destructure object -> peak the count property (picking it and storing in an another object called count)
+        return count == 0 ? <h1>Zero</h1> : count;
+        //jsx are normal javascript expression. can add headers instead of plain text too
     }
 }
  
