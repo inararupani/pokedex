5ed978be63562faf0e315be5b7537ef474ecf02c
Inara Rupani <inara.rupani@gmail.com>
2021-01-15 16:56:37 -0600

    Added: Binding Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,6 +15,25 @@ class Counter extends Component {
     };
     */
 
+    //Used to update the state property - Binding EventHandler to this (object)
+    /*
+    Every component add a constructor, call a base constructor, and for every event handler write code like this (this.handle)
+    */
+   /* constructor() {
+        //Call constructor of parent using keyowrd: super
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this); //return neww instance of handleIncrement instance
+        //this - references to current counter of the object
+    }*/
+
+    //convert this to Arrow Function- Simpler version of constructor/ rebinding every event handler mannually
+    handleIncrement = () => {
+        console.log('Increment Clicked!', this);
+        //When this is called in obj.method(); it will return a reference of function "method" to object "obj"
+        //function() - without object/standalone - wobject will be undefined
+    }
+
+
     renderTags() {
         if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
 
@@ -23,11 +42,14 @@ class Counter extends Component {
 
     render() { 
 
-        return <div>
-        
-            {this.state.tags.length === 0 && "Please create a new tag"}
-                {this.renderTags()}
-            </div>;
+        return (
+        //direct call to method
+        <div>
+            <span className={this.getBadgeClasses()}> {this.formatCount()} </span> 
+            <button onClick = {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+         </div>
+         //getting a string(tags) and mapping it with jsx expression which compiled through React Element = Plain Js obj 
+         //Each attribute need a key attribute for it to be dyanamically changed*/
 
         //since we are modifying the variable we use let, else use const
         //let classes = this.getBadgeClasses(); //appends to the className above
@@ -44,21 +66,10 @@ class Counter extends Component {
         <span style ={this.styles} className="badge badge-primary m-2"> {this.formatCount()} </span>  - from the function
         <span style ={{fontSize: 30}} className="badge badge-primary m-2"> {this.formatCount()} </span>
         */
-
-    
-       /*direct call to method
-        <div>
-            <span className={this.getBadgeClasses()}> {this.formatCount()} </span> 
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)} 
-            </ul>
-         </div>
-         //getting a string(tags) and mapping it with jsx expression which compiled through React Element = Plain Js obj 
-         //Each attribute need a key attribute for it to be dyanamically changed*/
+        )
     }
 
-    /*getBadgeClasses() {
+    getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary"; //appends to the className above
         return classes;
@@ -68,7 +79,7 @@ class Counter extends Component {
         const {count} = this.state; //destructure object -> peak the count property (picking it and storing in an another object called count)
         return count == 0 ? <h1>Zero</h1> : count;
         //jsx are normal javascript expression. can add headers instead of plain text too
-    }*/
+    }
 }
  
 export default Counter;
