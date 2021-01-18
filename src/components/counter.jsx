59335172d7c9d9d329c545849bf4d937b8e55812
Inara Rupani <inara.rupani@gmail.com>
2021-01-17 18:32:14 -0600

    Removed: local States

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     //dynamic object that have attributes
-    state  = {
+    //state  = {
         //count prints out the deafult value set by computer
-        count: this.props.counter.value
+      //  count: this.props.counter.value // only run when the counter instance is created
         //imageUrl: "https://picsum.photos/200" - randomly generated image
         //tags: [] 
 
@@ -12,7 +12,7 @@ class Counter extends Component {
         props: Includes data we give to a component. Read Only
         State: Includes data that is local or private. Other components cannot access that state. Completely Internal
         */
-    };
+    //};
 
     /*
     styles = {
@@ -33,35 +33,35 @@ class Counter extends Component {
     }*/
 
     //convert this to Arrow Function- Simpler version of constructor/ rebinding every event handler mannually
-    handleIncrement = (product) => {
+    //handleIncrement = (product) => {
         //console.log('Increment Clicked!', this);
         //When this is called in obj.method(); it will return a reference of function "method" to object "obj"
         //function() - without object/standalone - wobject will be undefined
         //console.log(product);
-        this.setState({count: this.state.count+1}); //Updating the state, and syncs the DOM with Virtual DOM
-    };
+      //  this.setState({count: this.state.count+1}); //Updating the state, and syncs the DOM with Virtual DOM
+   // };
 
     //so we can pass an argyment in the HandleIncremnent
     //when we call a function, we cant pass an argument so we create an extra function that wrapps to the original function with argument 
     //but is created as no arguments. Therefore we can use this fucntion in Onclick parameter
     //Not a good solution - cuz its clutter; instead use inline function
     
-    doHandleIncrement = () => {
-        this.handleIncrement({id: 1});
-    }
+    //doHandleIncrement = () => {
+      //  this.handleIncrement({id: 1});
+   // }
 
 
     //Conditional Rendering
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
+    //renderTags() {
+      //  if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
 
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    }
+      //  return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    //}
 
     render() { 
         //console.log('props', this.props);
         //props - js componment: they have the attribites of counter componnets
-        console.log(this.props);
+        //console.log(this.props);
 
         return (
         //direct call to method
@@ -69,8 +69,18 @@ class Counter extends Component {
         //and in paramenters dont hardcode
         <div>
             <span className={this.getBadgeClasses()}> {this.formatCount()} </span> 
-            <button onClick = {this.doHandleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
-            <button onClick = {() => this.props.onDelete(this.props.counter.id)} className= "btn btn-danger btn-small m-2">Delete</button>
+            <button 
+            onClick = {() => this.props.onIncrement(this.props.counter)} 
+            className = "btn btn-secondary btn-sm"
+            >
+                Increment
+            </button>
+            <button 
+            onClick = {() => this.props.onDelete(this.props.counter.id)} 
+            className= "btn btn-danger btn-small m-2"
+            >
+                Delete
+            </button>
         </div>
          //getting a string(tags) and mapping it with jsx expression which compiled through React Element = Plain Js obj 
          //Each attribute need a key attribute for it to be dyanamically changed*/
@@ -95,13 +105,13 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary"; //appends to the className above
+        classes += (this.props.counter.value === 0) ? "warning" : "primary"; //appends to the className above
         return classes;
     }
 
     formatCount(){
-        const {count} = this.state; //destructure object -> peak the count property (picking it and storing in an another object called count)
-        return count === 0 ? <h1>Zero</h1> : count;
+        const {value} = this.props.counter; //destructure object -> peak the count property (picking it and storing in an another object called count)
+        return value === 0 ? <h1>Zero</h1> : value;
         //jsx are normal javascript expression. can add headers instead of plain text too
     }
 }
