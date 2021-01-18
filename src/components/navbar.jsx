8366aca2d112a83fdf6d14d6428a52cc3a8419cd
Inara Rupani <inara.rupani@gmail.com>
2021-01-17 19:35:09 -0600

    Added: Stateless Functional Component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-class NavBar extends Component {
-    render() { 
-        return ( <nav className="navbar navbar-light bg-light">
+
+/*
+    Stateless functional Component (sfc): instead of using props, states, and classes. We use functions
+    Instead of having a class that extends a component and have a render method. We have a const function that return a react elememt
+    have to add "props" as a paramter in the function, React will add props element at RunTime.
+*/
+
+const NavBar = (props) => {
+
+    return ( 
+    <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-          Navbar {" "} <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+          Navbar {" "} <span className="badge badge-pill badge-secondary">
+              {props.totalCounters}</span>
           </a>
         </div>
-      </nav> );
-    }
-}
- 
+      </nav> 
+    
+    );
+};
+
 export default NavBar;
